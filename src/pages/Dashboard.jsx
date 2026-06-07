80ad839e2ddbd639bf3e0e53d90a2001f7a876f8
meskbh93-ctrl <meskbh93@gmail.com>
2026-06-07 12:55:51 +0300

    Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,9 @@
-
 export default function Dashboard() {
-  return <div>Dashboard Page</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">
+        Dashboard
+      </h1>
+    </div>
+  );
 }
