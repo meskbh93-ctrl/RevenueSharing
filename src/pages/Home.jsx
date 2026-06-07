03dbac4dd83078c548ace9d0ba2fd869c7c3e565
meskbh93-ctrl <meskbh93@gmail.com>
2026-06-07 12:53:52 +0300

    Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,11 @@
-
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">
+        Home
+      </h1>
+
+      <p>Welcome to Revenue Sharing App ✅</p>
+    </div>
+  );
 }
