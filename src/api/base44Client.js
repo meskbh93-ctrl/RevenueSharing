b9d37969b83f6c0050b07a0d8c84d7a190805ffa
meskbh93-ctrl <meskbh93@gmail.com>
2026-06-08 13:39:57 +0300

    base44Client.js

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -4,62 +4,34 @@ export const base44 = {
     logout: () => {},
     redirectToLogin: () => {},
   },
-
   entities: {
     Project: {
-      create: async (data) => ({
-        id: Date.now(),
-        ...data,
-      }),
-
-      update: async (id, data) => ({
-        id,
-        ...data,
-      }),
-
+      create: async (data) => ({ id: Date.now(), ...data }),
+      update: async (id, data) => ({ id, ...data }),
+      delete: async () => true,
       list: async () => [],
+      filter: async () => [],
     },
-
     Service: {
-      create: async (data) => ({
-        id: Date.now(),
-        ...data,
-      }),
-
-      update: async (id, data) => ({
-        id,
-        ...data,
-      }),
-
+      create: async (data) => ({ id: Date.now(), ...data }),
+      update: async (id, data) => ({ id, ...data }),
+      delete: async () => true,
       list: async () => [],
+      filter: async () => [],
     },
-
     Cost: {
-      create: async (data) => ({
-        id: Date.now(),
-        ...data,
-      }),
-
-      update: async (id, data) => ({
-        id,
-        ...data,
-      }),
-
+      create: async (data) => ({ id: Date.now(), ...data }),
+      update: async (id, data) => ({ id, ...data }),
+      delete: async () => true,
       list: async () => [],
+      filter: async () => [],
     },
-
     IncomeSharing: {
-      create: async (data) => ({
-        id: Date.now(),
-        ...data,
-      }),
-
-      update: async (id, data) => ({
-        id,
-        ...data,
-      }),
-
+      create: async (data) => ({ id: Date.now(), ...data }),
+      update: async (id, data) => ({ id, ...data }),
+      delete: async () => true,
       list: async () => [],
+      filter: async () => [],
     },
   },
 };
