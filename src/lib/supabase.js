06e2836b7697e345c486101d804a78ff17b4fba4
meskbh93-ctrl <meskbh93@gmail.com>
2026-06-09 10:13:02 +0300

    supabase.js

---
+++ src/lib/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl =
+  'https://enptxiziggtceiewqwie.supabase.co/rest/v1/'
+
+const supabaseKey =
+  'sb_publishable_fhTXOZFb6wGi4hZy1xT4Tw_MhUnqyOs'
+
+export const supabase =
+  createClient(
